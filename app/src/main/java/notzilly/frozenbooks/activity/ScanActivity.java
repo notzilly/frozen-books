417cc3f0f7b49a33e6fdc051bcc8b599911eee99
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2018-06-22 00:42:06 -0300

    ScanActivity scans qr code then returns its value via intent

--- app/src/main/java/notzilly/frozenbooks/activity/ScanActivity.java
+++ app/src/main/java/notzilly/frozenbooks/activity/ScanActivity.java
@@ -1,14 +1,19 @@
 package notzilly.frozenbooks.activity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
@@ -62,7 +67,25 @@ public class ScanActivity extends AppCompatActivity {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
 
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if(barcodes.size() > 0) {
+                    Intent intent = new Intent();
+                    intent.putExtra("barcode", barcodes.valueAt(0));
+                    setResult(CommonStatusCodes.SUCCESS, intent);
+                    finish();
+                }
             }
         });
     }
