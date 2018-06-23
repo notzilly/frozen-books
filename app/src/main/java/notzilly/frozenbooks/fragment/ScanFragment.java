3e012ba765ae0830f1a0bbfb7c2a44b76498389c
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2018-06-22 23:10:35 -0300

    Button starts activity and opens scanner correctly

--- app/src/main/java/notzilly/frozenbooks/fragment/ScanFragment.java
+++ app/src/main/java/notzilly/frozenbooks/fragment/ScanFragment.java
@@ -1,18 +1,28 @@
 package notzilly.frozenbooks.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import notzilly.frozenbooks.R;
+import notzilly.frozenbooks.activity.ScanActivity;
 
-public class ScanFragment extends Fragment {
+public class ScanFragment extends Fragment implements View.OnClickListener {
 
     // Required empty public constructor
     public ScanFragment() {}
+
+    // Click event for opening scanner
+
+    public void scanBarcode(View v){
+        Intent intent = new Intent(getActivity(), ScanActivity.class);
+        startActivityForResult(intent, 0);
+    }
 
     // Factory of ScanFragment
     public static ScanFragment newInstance() {
@@ -26,8 +36,19 @@ public class ScanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_scan, container, false);
+
+        Button openScanner = (Button) v.findViewById(R.id.open_scanner);
+        openScanner.setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        scanBarcode(v);
     }
 
     @Override
