a9533595313ac1b960e3a1312013318761faad41
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2018-07-09 16:22:37 -0300

    ListFreezersFragment is going to use a recycler and an adapter to display content

--- app/src/main/java/notzilly/frozenbooks/fragment/ListFreezersFragment.java
+++ app/src/main/java/notzilly/frozenbooks/fragment/ListFreezersFragment.java
@@ -1,41 +1,57 @@
 package notzilly.frozenbooks.fragment;
 
-
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import notzilly.frozenbooks.R;
+import notzilly.frozenbooks.model.Freezer;
+import notzilly.frozenbooks.viewholder.FreezerViewHolder;
 
 public class ListFreezersFragment extends Fragment {
+
+    private DatabaseReference db;
+    private FirebaseRecyclerAdapter<Freezer, FreezerViewHolder> fbAdapter;
+    private RecyclerView recycler;
+    private LinearLayoutManager manager;
 
     // Required empty public constructor
     public ListFreezersFragment() {}
 
+    // Static method to instantiate a new fragment
     public static ListFreezersFragment newInstance() {
         ListFreezersFragment fragment = new ListFreezersFragment();
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_freezers, container, false);
+        View v = inflater.inflate(R.layout.fragment_list_freezers, container, false);
+
+        // Gets database reference
+        db = FirebaseDatabase.getInstance().getReference();
+
+        // Finds recycler and sets fixed size
+        recycler = v.findViewById(R.id.freezers_list);
+        recycler.setHasFixedSize(true);
+
+        return v;
     }
 
     @Override
-    public void onAttach(Context context) { super.onAttach(context); }
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-    @Override
-    public void onDetach() { super.onDetach(); }
 
+    }
 }
